efc18a49e84a873a3dbd85a74012fabe0db138c9
turbillon50 <turbillon50@gmail.com>
2026-04-27 02:21:20 -0500

    Add Stripe account API route

---
+++ api/stripe/account.js
@@ -0,0 +1,11 @@
+const Stripe = require('stripe');
+
+module.exports = async (req, res) => {
+  try {
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+    const account = await stripe.accounts.retrieve();
+    return res.status(200).json(account);
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
